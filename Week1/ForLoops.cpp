5ef04a1f18dfc6989cac683d906d1e8acf49cd17
Shikhar2929 <66570303+Shikhar2929@users.noreply.github.com>
2021-09-30 18:07:52 -0400

    Add files via upload

---
+++ Week1/ForLoops.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+// This is how you comment 
+//iostream and using namespace std are fundamental libraries 
+
+int main()
+{
+	for (int i = 0;i < 5;i++)
+	{
+		//Define index, ending value, increment 
+		cout << i << endl;
+	}
+
+
+
+}
+
