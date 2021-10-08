f35a556f9d01f36a9eaebbdc9b55009bb111c7c4
Aarush Gupta <51494575+ag2718@users.noreply.github.com>
2021-10-08 11:07:03 -0400

    Create teleport.cpp

---
+++ Week2/teleport.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+  // This connects the files to standard input and output
+  freopen("teleport.in", "r", stdin);
+  freopen("teleport.out", "w", stdout);
+
+  // Reading in input and initializing values
+  int a, b, x, y;
+  cin >> a >> b >> x >> y;
+
+  // Finding the minimum of three potential paths between positions a and b
+  cout << min(abs(a - b),
+              min(abs(a - x) + abs(b - y), abs(b - x) + abs(a - y)));
+
+  return 0;
+}
